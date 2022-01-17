5b55a274f2629f4979474f11bf91e05d76009bd2
ignasc <ignui.c@gmail.com>
2022-01-17 20:59:49 +0000

    Heat map is being generated.
    Axis, color legend missing. And some cosmetics needed too.

--- mainJS.js
+++ mainJS.js
@@ -8,14 +8,30 @@ document.addEventListener("DOMContentLoaded", function(){
        req.send();
        req.onload = function(){
               const jsonDATA = JSON.parse(req.responseText);
-              console.log(jsonDATA);
               
-              const chartWidth = 800;
-              const chartHeight = 500;
               const pageWidth  = document.documentElement.scrollWidth;
+              const chartWidth = pageWidth<1024? 1024:pageWidth*0.8;
+              const chartHeight = 500;
               const padding = 60;
 
+              /*Some functions for returning proper data*/
+              const returnYear = (jsonObject) => {
+                     return jsonObject["year"];
+              };
+              const returnMonth = (jsonObject) => {
+                     return jsonObject["month"];
+              };
+              const returnVariance = (jsonObject) => {
+                     return jsonObject["variance"];
+              };
+
               /*DATA PROCESSING FOR HEAT MAP*/
+              const dataArray = [...jsonDATA["monthlyVariance"]];
+              /*const tempDataArray = [
+                     jsonDATA["monthlyVariance"][0],
+                     jsonDATA["monthlyVariance"][50],
+                     jsonDATA["monthlyVariance"][500]
+              ];*/
               
               /*DATA PROCESSING FOR HEAT MAP END*/
 
@@ -44,6 +60,50 @@ document.addEventListener("DOMContentLoaded", function(){
               .attr("y", padding/2)
               .attr("id", "title")
               .attr("text-anchor", "middle");
+
+              /*Define main heat map scales*/
+              
+             const heatMapXScale = d3.scaleLinear()
+                     .domain([d3.min(dataArray, (data)=>data["year"]),
+                     d3.max(dataArray, (data)=>data["year"])])
+                     .range([padding, chartWidth - padding]);
+
+              const heatMapYScale = d3.scaleLinear()
+                     .domain([13, 0])
+                     .range ([chartHeight - padding, padding]);
+
+              /*scale for temperature indication as color*/
+              const temperatureColorScale = d3.scaleLinear()
+              .domain([d3.min(dataArray, (data)=>data["variance"]),
+              d3.max(dataArray, (data)=>data["variance"])])
+              /*color hue range yellow to red (60 - 0)*/
+              .range([60, 0]);
+
+              /*let's calculate heat map bar width and height*/
+              const barWidth = (chartWidth-padding*2)/(d3.max(dataArray, (data)=>data["year"]) - d3.min(dataArray, (data)=>data["year"])) + "px";
+              const barHeight =  (chartHeight-padding*2)/12 + "px";
+
+              const setTempColor = (tempVariance)=>{
+                     return "hsl(" + temperatureColorScale(tempVariance) + ", 100%, 50%";
+              };
+
+              svg.selectAll("rect")
+                     .data(dataArray)
+                     .enter()
+                     .append("rect")
+                     .attr("id", "heat-map-bar")
+                     .attr("width", barWidth)
+                     .attr("height", barHeight)
+                     .style("fill", (data)=>setTempColor(data["variance"]))
+                     .attr("x", (data)=>{
+                            return heatMapXScale(data["year"]);
+                     })
+                     .attr("y", (data)=>{
+                            return heatMapYScale(data["month"]);
+                     });
+             
+             /*DEBUG*/
+              console.log(dataArray[0]);
 
 
        };
